1f4434f80aea36edd0b2764fd0d945cc033296d7
unknown <cogjava4358@iiht.tech>
2022-11-10 15:19:41 +0530

    Junit examples

--- JUnitDemo/src/com/junit/demo/Calculator.java
+++ JUnitDemo/src/com/junit/demo/Calculator.java
@@ -6,15 +6,18 @@ public static void main(String[] args) {
 }
 	public int add(int n1,int n2)
 	{
-	return n1+n2;
+	int total =n1+n2;
+	return total;
 	}
 	public int sub(int n1,int n2)
 	{
-	return n1-n2;
+	int total = n1-n2;
+	return total;
 	}
 	public int multiply(int n1,int n2)
 	{
-	return n1*n2;
+	int total =n1*n2;
+	return total;
 	}
 	
 }
